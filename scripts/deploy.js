8812f91dd54149d2494e95f19d09d0818d13974c
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-06-05 17:47:55 +0900

    minting
    mint nft

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,15 +8,25 @@ const main = async () => {
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
     let txn;
-    // We only have three characters.
-    // an NFT w/ the character at index 2 of our array.
+    txn = await gameContract.mintCharacterNFT(0);
+    await txn.wait();
+    console.log("Minted NFT #1");
+  
+    txn = await gameContract.mintCharacterNFT(1);
+    await txn.wait();
+    console.log("Minted NFT #2");
+  
     txn = await gameContract.mintCharacterNFT(2);
     await txn.wait();
-
-    // Get the value of the NFT's URI.
-    let returnedTokenUri = await gameContract.tokenURI(1);
-    console.log("Token URI:", returnedTokenUri);
-    };
+    console.log("Minted NFT #3");
+  
+    txn = await gameContract.mintCharacterNFT(1);
+    await txn.wait();
+    console.log("Minted NFT #4");
+  
+    console.log("Done deploying and minting!");
+  
+  };
   
   const runMain = async () => {
     try {
@@ -29,6 +39,3 @@ const main = async () => {
   };
   
   runMain();
-
-
-  
